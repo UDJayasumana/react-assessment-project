c8bbf8cd328d5e6145a8f0252fe5e2e27d8397c6
UDJayasumana <uartcreations@gmail.com>
2025-12-14 18:17:59 +0530

    Create the api-endpoint js file for our product apis

---
+++ src/api/endpoints/product.endpoint.js
@@ -0,0 +1,7 @@
+export const PRODUCT_ENDPOINTS = {
+    LIST: '/products/',
+    DETAILS: (id) => `/products/${id}`,
+    CREATE: '/products/',
+    UPDATE: (id) => `/products/${id}`,
+    PATCH: (id) => `/products/${id}`
+}
