b49afc598af4abc5a3e4d521593eb37c1e703845
UDJayasumana <uartcreations@gmail.com>
2025-12-14 16:36:19 +0530

    Add Sidebar abd Navbar to the layout

---
+++ src/components/main/Sidebar.jsx
@@ -0,0 +1,59 @@
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from "@mui/material";
+import React from "react";
+
+const Sidebar = ({ width, menuItems, onClickItem }) => {
+  const theme = useTheme();
+ 
+
+  return (
+    <Drawer
+      sx={{
+        width: width,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: width,
+          backgroundColor: "#EBEDDA",
+          mt: `${theme.mixins.toolbar.minHeight}px`,
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Box>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            padding: 2,
+            fontFamily: '"Poppins", sans-serif',
+            fontWeight: 600,
+          }}
+        >
+          Courier Flex
+        </Typography>
+
+        <List>
+          {menuItems.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton
+                  onClick={() => onClickItem(item.path)}
+                  sx={{
+                    background: isActive ? "#f4f4f4" : null,
+                    "&:hover": { background: isActive ? "#f4f4f4" : null },
+                  }}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import { Dashboard } from "@mui/icons-material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Dashboard /> }
+    ],
+  },
+]);
+
+const theme = createTheme();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
